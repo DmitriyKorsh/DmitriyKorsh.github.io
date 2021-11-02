a15512e8170816514e82b7b89531e73a08a7dffc
DmitriyKorsh <93598872+DmitriyKorsh@users.noreply.github.com>
2021-11-02 18:00:59 +0300

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/4.jpg') {
+      myImage.setAttribute ('src','images/6.jpg');
+    } else {
+      myImage.setAttribute ('src','images/4.jpg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Пожалуста, укажите, как к вам обращаться: ');
+  if(!myName) {
+    setUserName();
+  } else {
+    localStorage.setItem('name', myName);
+    myHeading.innerHTML = 'Погружение в фентези мир, в который хочется верить! Верно, ' + storedName + '?';
+  }
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Погружение в фентези мир, в который хочется верить! Верно, ' + storedName + '?';
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
