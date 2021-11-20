eae196c52f438b15289db4d7a6a271cfefc07d64
DmitriyKorsh <93598872+DmitriyKorsh@users.noreply.github.com>
2021-11-20 13:58:45 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -8,27 +8,3 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/6.jpg');
     }
 }
-
-let myButton = document.querySelector('button');
-let myHeading = document.querySelector('h1');
-
-function setUserName() {
-  let myName = prompt('Пожалуста, укажите, как к вам обращаться: ');
-  if(!myName) {
-    setUserName();
-  } else {
-    localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Почему все так фанатеют от Ведьмака? Интересно, ' + storedName + '?';
-  }
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Почему все так фанатеют от Ведьмака? Интересно, ' + storedName + '?';
-}
-
-myButton.onclick = function() {
-  setUserName();
-}
